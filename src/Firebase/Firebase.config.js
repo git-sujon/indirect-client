0488f9cf4bb0bfcc2a6cae45a1016f2fadb5b5f0
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-24 01:53:55 +0600

    Navbar added

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQms22UWFxGhXeAv6EIUMAUhybKazWWhs",
+  authDomain: "indirect-fb2af.firebaseapp.com",
+  projectId: "indirect-fb2af",
+  storageBucket: "indirect-fb2af.appspot.com",
+  messagingSenderId: "348710316127",
+  appId: "1:348710316127:web:607972fb1af1e26afc93ea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
