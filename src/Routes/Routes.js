6e379d7de8ed6711eb63efe38c74f8213a4a5e12
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-24 02:21:43 +0600

    Error page created

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Home from '../Pages/Home/Home';
 
 const Routes = () => {
-    
+    const router = createBrowserRouter([
+        {
+            path:'/',
+            element:<Main></Main>,
+            errorElement:<ErrorPage></ErrorPage>,
+            children:[
+                {
+                    path:'/',
+                    element:<Home></Home>
+                }
+            ]
+        }
+    ])
 };
 
 export default Routes;
