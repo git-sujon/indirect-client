79ae292761ae90172735b77eda74a07222b62c6f
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-27 01:16:54 +0600

    Products query added

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,19 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import ProductCard from './ProductCard';
 
 const Products = () => {
- 
+    const category= useLoaderData()
+   
     
     const { data: products = [], isLoading } = useQuery({
-        queryKey: ["products"],
+        queryKey: ["products", category?.catagoriesName],
         queryFn: async () => {
-          const res = await fetch(`http://localhost:5000/products`);
+          const res = await fetch(`http://localhost:5000/products?category=${category.catagoriesName}`);
           const data = await res.json();
           return data;
         },
       });
-      console.log(products);
+     
 
     return (
         <div className='container px-5 mx-auto my-20'>
