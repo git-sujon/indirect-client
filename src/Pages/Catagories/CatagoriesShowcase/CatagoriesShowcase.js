49a2d864959e02937138b5e10082306b168253f6
Mr-Robot-101 <git.sujon@gmail.com>
2022-12-31 20:43:10 +0600

    HEader Added to  the categories section

--- src/Pages/Catagories/CatagoriesShowcase/CatagoriesShowcase.js
+++ src/Pages/Catagories/CatagoriesShowcase/CatagoriesShowcase.js
@@ -21,7 +21,10 @@ if(isLoading){
 }
 
     return (
-        <div className='my-20'>
+        <div className='pt-32'>
+               <h2 className="text-center text-4xl font-semibold  mb-20 text-gray-700">
+            Explore More
+          </h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
             {
                 catagories?.data?.map(category => <CatagoriesCard key={category._id} category={category} ></CatagoriesCard>)
