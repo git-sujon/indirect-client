532f2d6706f446fda91bec95aa597a448e7df2d4
Mr-Robot-101 <git.sujon@gmail.com>
2023-01-09 00:18:28 +0600

    WHy Indirect section addded

---
+++ src/Pages/Home/WhyIndirect/WhyIndirect.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const WhyIndirect = () => {
+  return (
+    <section class="overflow-hidden sm:grid sm:grid-cols-2 my-32">
+      <img
+        alt="Student"
+        src="https://static.standard.co.uk/homesandproperty/s3fs-public/thumbnails/image/2019/04/16/10/EasterBuyshutterstock-1156208527.jpg?width=968&auto=webp&quality=50&crop=968%3A645%2Csmart"
+      />
+      <div class=" pl-8 pb-8 md:pb-0">
+        <div class="mx-auto max-w-xl text-center sm:text-left">
+          <h2 class="text-3xl font-bold text-gray-900 md:text-6xl">
+            Our Agent Can Help You Find the Best House
+          </h2>
+
+          <p class="hidden text-gray-600 md:mt-4 md:block">
+            Allow our skilled agent to assist you in your search for the perfect
+            home. With their expertise and dedication to finding the best fit
+            for you and your family, you can trust that you will be guided
+            towards the house of your dreams. Don't waste any more time scouring
+            listings on your own, let our agent handle the hard work and help
+            you find the best house on the market.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyIndirect;
