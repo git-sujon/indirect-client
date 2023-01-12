8190785521c30c506eaaee851900425fd142fdc2
Mr-Robot-101 <git.sujon@gmail.com>
2023-01-12 23:09:03 +0600

    Counter responsive issue fix

--- src/Pages/Home/Counter/Counter.js
+++ src/Pages/Home/Counter/Counter.js
@@ -9,7 +9,7 @@ import {
 const Counter = () => {
   return (
     <div className="text-white bg-[#072f60] ">
-      <div className="py-5 px-4 md:px-10 flex gap-y-5 divide-y-2 md:divide-y-0 divide-gray-400 flex-col md:flex-row items-center justify-between">
+      <div className="py-5 px-4 md:px-10 flex items-center justify-between">
         {/* part 1  */}
         <div className="flex items-center gap-x-5">
           <div
@@ -17,14 +17,14 @@ const Counter = () => {
             data-aos-easing="linear"
             data-aos-duration="1000"
           >
-            <HomeIcon className="w-20"></HomeIcon>
+            <HomeIcon className="w-10 md:w-20"></HomeIcon>
           </div>
           <div
             data-aos="slide-up"
             data-aos-easing="linear"
             data-aos-duration="1000"
           >
-            <p className="text-6xl font-bold">
+            <p className="text-xl md:text-4xl lg:text-6xl font-bold">
               <CountUp start={0} end={4234} delay={3} />+
             </p>
             <p className="text-xs">Homes are already sold</p>
@@ -38,14 +38,14 @@ const Counter = () => {
             data-aos-easing="linear"
             data-aos-duration="1000"
           >
-            <BuildingOfficeIcon className="w-20"></BuildingOfficeIcon>
+            <BuildingOfficeIcon className="w-10 md:w-20"></BuildingOfficeIcon>
           </div>
           <div
             data-aos="slide-up"
             data-aos-easing="linear"
             data-aos-duration="1000"
           >
-            <p className="text-6xl font-bold">
+            <p className="text-xl md:text-4xl lg:text-6xl font-bold">
               <CountUp start={0} end={2348} delay={3} />+
             </p>
             <p className="text-xs">Apartments are already sold</p>
@@ -59,14 +59,14 @@ const Counter = () => {
             data-aos-easing="linear"
             data-aos-duration="1000"
           >
-            <HomeModernIcon className="w-20"></HomeModernIcon>
+            <HomeModernIcon className="w-10 md:w-20"></HomeModernIcon>
           </div>
           <div
             data-aos="slide-up"
             data-aos-easing="linear"
             data-aos-duration="1000"
           >
-            <p className="text-6xl font-bold">
+            <p className="text-xl md:text-4xl lg:text-6xl font-bold">
               <CountUp start={0} end={3423} delay={1} />+
             </p>
             <p className="text-xs text-justify">
